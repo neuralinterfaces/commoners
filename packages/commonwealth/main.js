951e2fe89cd319f8a5f7c7e87e5f515446030ddd
Garrett <gflynn@usc.edu>
2023-04-04 18:38:53 -0700

    Clean up the test server

--- packages/commonwealth/main.js
+++ packages/commonwealth/main.js
@@ -1,39 +1,31 @@
 import express from 'express'
 import cors from 'cors'
-import * as commonwealth from './src/index.js'
 import * as object from './demo/object.js'
 import { HTTPServer } from './src/http/server.js'
 
-const app = express()
-app.use(cors())
+const port = 3768 // Specify the app port
+
+// --------------- Setup Express + Middleware ---------------
+const app = express() // Create the Express app
+app.use(cors()) // Allow CORS
 app.use(express.json()) // Support JSON-encoded bodies
 app.use(express.text()) // Support text bodies
-
-const name = 'object'
-
-const http = new HTTPServer()
-
-http.service.add(name, object)
-// service.add(object) // Add object to load as a service
-
-const port = 3768
-const thisUrl = `http://localhost:${port}`
-
 app.use(express.static('.')) // Serve static files from the current directory
 
-app.get('/events', http.subscribe)
-app.post('/subscribe', http.setSubscription)
+// --------------- Create your Service ---------------
+const http = new HTTPServer() // Create an HTTP Server Service
+http.service.add('object', object) // Mirror the client endpoint
 
-app.post('/echo', (req, res) => {
-    console.log('Echoed', req.body)
-    res.send(req.body)
-})
+// --------------- SSE Handlers ---------------
+app.get('/events', http.subscribe) // Ensure SSE initialization is handled
+app.post('/subscribe', http.setSubscription) // Ensure subscriptions are handled
 
+// --------------- Non-Service Handlers ---------------
+app.post('/echo', (req, res) => res.send(req.body)) // Specify a route not handled by the service
 
-// Handle anything not covered by the static paths using get requests to the service
-app.get('*', http.get)
+// --------------- Generic Service Handlers ---------------
+app.get('*', http.get) // Handle anything not covered by the static paths using get requests to the service
+app.post('*', http.post) // Handle all post requests with the service
 
-// Handle all post requests with the service
-app.post('*', http.post)
-
-app.listen(port, () => console.log(`Server started at ${thisUrl}`))
+// --------------- Run the Server ---------------
+app.listen(port, () => console.log(`Server started at http://localhost:${port}`))
